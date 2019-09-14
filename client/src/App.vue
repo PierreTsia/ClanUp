b2b7da4580a5737dc3f57dc931acc60588d91d64
Pierre_T <pierre.tsiakkaros@gmail.com>
2019-09-14 20:33:02 +0200

    :boom: generate client folder

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar app dark>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Clan-Up</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2 secondary--text">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+  data: () => ({
+    //
+  })
+});
+</script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap>
+      test
+    </v-layout>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Home",
+  components: {}
+});
+</script>
