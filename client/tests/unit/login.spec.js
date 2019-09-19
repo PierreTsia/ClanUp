74f5d8d948b7bf8ee73b09d890179dff057ea513
Pierre_T <pierre.tsiakkaros@gmail.com>
2019-09-19 23:04:08 +0200

    :white_check_mark: Login.vue unit tests

--- client/tests/unit/login.spec.js
+++ client/tests/unit/login.spec.js
@@ -1,4 +1,4 @@
-import { mount, shallowMount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import Login from "@/views/Login.vue";
 import Vue from "vue";
 import Vuetify from "vuetify";
@@ -10,14 +10,14 @@ describe("Login.vue", () => {
     expect(wrapper).toBeDefined();
   });
 
-  it("should have an active component id set to sign up", () => {
+  it("should display active login component and toggle signup/signin", () => {
     const wrapper = shallowMount(Login);
+    const componentRef1 = wrapper.find(".login__card");
     expect(wrapper.vm.$data.activeComponentId).toEqual("signup-form");
-
-    console.log(wrapper.vm.$data.activeComponentId);
-    /*const textFields = wrapper.findAll(".v-text-field");
-    expect(textFields.length).toEqual(6);
-    expect(textFields.at(0).props().label).toBe("E-mail");
-    expect(textFields.at(1).props().label).toBe("Password");*/
+    expect(componentRef1.name()).toEqual("SignupForm");
+    wrapper.vm.handleFormChange({ id: "signin-form" });
+    const componentRef2 = wrapper.find(".login__card");
+    expect(wrapper.vm.$data.activeComponentId).toEqual("signin-form");
+    expect(componentRef2.name()).toEqual("SigninForm");
   });
 });
